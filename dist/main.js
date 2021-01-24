31e5060a0ff6efbd283abee32d19ede12f99d635
zuiwuchang <zuiwuchang@gmail.com>
2021-01-24 16:10:30 +0800

    init

---
+++ dist/main.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var version_1 = require("./version");
+console.log("this is main test : " + version_1.Version);
---
+++ dist/version.d.ts
@@ -0,0 +1,1 @@
+export declare const Version = "0.0.1";
